c19cc2f9b7ea0c2fda2f378ee7917408ad70fa1c
avasherry <77854326+avasherry@users.noreply.github.com>
2021-03-01 17:28:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -26,32 +26,32 @@ import time
 def summaryFWD():
     dbc_filename = '/Users/asherry/Documents/rideable-firmware-master/api/can_dbc/cosmo.dbc'
     # asc_filename = '/Users/asherry/Desktop/TestRideData/VAL/logfile010.asc'
-    asc_filename = '/Users/asherry/Desktop/TestRideData/REL/Second_Distance_Track/REL13_100km/logfile2021-01-27_10-05-12.asc'
-    can_logs = LyftCAN.ascToDataframe(asc_filename, dbc_filename)
-    can_log_by_signal = can_logs['canlogs_by_signal']
+    asc_filename = '/Users/asherry/Desktop/TestRideData/REL/Second_Distance_Track/REL13_100km/logfile2021-01-27_03-32-34.asc'
+    # asc_filename = '/Users/asherry/Desktop/Thien-Ride-20210218_1600_CAN.asc'
+    db = cantools.database.load_file(dbc_filename)
 
+    filteredSignals = ["MC_DiagnosticMatrix", "MC_FaultMatrix", "MC_WarningMatrix", "BMS_DiagnosticMatrix", "BMS_FaultMatrix", "BMS_WarningMatrix"]
+    filteredMsgID = set()
+
+    for signal in filteredSignals:
+        msg = db.get_message_by_name(signal)
+        filteredMsgID.add(msg.frame_id)
+
+
+    can_logs = LyftCAN.ascToDataframe(asc_filename, dbc_filename, filteredMsgID)
+    can_log_by_signal = can_logs['canlogs_by_signal']
 
     for signal in can_log_by_signal:
         count = 0
-        s = signal.split("_")  ## "MC_f032_TorqueInputRationality" --> ["MC", "f032", "TorqueInputRationality"]
-        if s[1][0] in ['w', 'f', 'd']:  ## is the signal in a fault/warning/diagnostic matrix (ex. MC_w001)
-            log = can_log_by_signal[signal]
-            for y in range(0, len(log)):
-                if log.Object[y] == 1:
-                    count += 1
-            if count >= 1:
-                print(str(count) + " :" + signal)
+        log = can_log_by_signal[signal]
+        for y in range(0, len(log)):
+            if log.Object[y] == 1:
+                count += 1
+        if count >= 1:
+            print(str(count) + " :" + signal)
 
 
-# input2 = input('Decode? (y/n) ')
-# if input2 == 'y':
-# inputVAL1 = input('Which faults are you interested in (MC/BMS)? ')
-# else:
-
-
-input3 = input('Summary? (y/n) ')
-if input3 == 'y':
-    start_time = time.time()
+input1 = input('Summary? (y/n) ')
+if input1 == 'y':
     summaryFWD()
-    print("My program took", time.time() - start_time, " seconds to run")
 
